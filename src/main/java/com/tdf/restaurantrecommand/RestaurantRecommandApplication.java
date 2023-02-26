5143d3784ff916110c8706642cdf81019ac1eb59
Neeraj Suthar <suthar.nrj@gmail.com>
2023-02-26 13:12:42 +0530

    Initial Commit

---
+++ src/main/java/com/tdf/restaurantrecommand/RestaurantRecommandApplication.java
@@ -0,0 +1,13 @@
+package com.tdf.restaurantrecommand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantRecommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantRecommandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tdf/restaurantrecommand/RestaurantRecommandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdf.restaurantrecommand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantRecommandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
